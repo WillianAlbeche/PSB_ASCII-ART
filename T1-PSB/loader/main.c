6d18fed0cc640b506e842f73357abdae5d17a066
willian.albeche <willian.albeche@edu.pucrs.br>
2020-10-21 23:36:16 -0300

    atualização de alguns codigos espeficicos na main

--- T1-PSB/loader/main.c
+++ T1-PSB/loader/main.c
@@ -4,7 +4,11 @@
 #include <stdbool.h>
 // SOIL é a biblioteca para leitura das imagens
 #include "SOIL.h"
+#include <math.h>
 
+typedef struct {
+    int width, height, totalSize;
+} Dimension; 
 // Um pixel RGB
 typedef struct {
     unsigned char r, g, b;
@@ -12,9 +16,30 @@ typedef struct {
 
 // Uma imagem em RGB
 typedef struct {
-    int width, height;
+    Dimension dimension;
+    //int width, height;
     RGB* img;
 } Img;
+
+void reduceToFrom(Img* reducedPic, Img* realPic);
+
+void reduceToFrom(Img* reducedPic, Img* realPic) {
+    printf("Reduzindo img\n");
+    reducedPic->img[reducedPic->dimension.totalSize - 1] = realPic->img[realPic->dimension.totalSize - 1];
+    int realPicLinePosition = 0;
+    int realPicColumnPosition = 0;
+    RGB (* reducedImage)[reducedPic->dimension.width] = (RGB(*)[reducedPic->dimension.width]) reducedPic->img;
+    RGB (* realImage)[realPic->dimension.width] = (RGB(*)[realPic->dimension.width]) realPic->img;
+    float widthRelation =  (realPic->dimension.width / (float) reducedPic->dimension.width);
+    float heightRelation =  (realPic->dimension.height / (float) reducedPic->dimension.height);
+    for(int line = 0; line < reducedPic->dimension.height - 1; line++) {
+        realPicLinePosition = (int) round(heightRelation * line);
+        for(int column = 0; column < reducedPic->dimension.width; column++) {
+            realPicColumnPosition = (int) round(widthRelation * column);
+            reducedImage[line][column] = realImage[realPicLinePosition][realPicColumnPosition];
+        }
+    }
+}
 
 // Protótipos
 void load(char* name, Img* pic);
@@ -23,13 +48,13 @@ void load(char* name, Img* pic);
 void load(char* name, Img* pic)
 {
     int chan;
-    pic->img = (unsigned char*) SOIL_load_image(name, &pic->width, &pic->height, &chan, SOIL_LOAD_RGB);
+    pic->img = (unsigned char*) SOIL_load_image(name, &pic->dimension.width, &pic->dimension.height, &chan, SOIL_LOAD_RGB);
     if(!pic->img)
     {
         printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
         exit(1);
     }
-    printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
+    printf("Load: %d x %d x %d\n", pic->dimension.width, pic->dimension.height, chan);
 }
 
 int main(int argc, char** argv)
@@ -49,7 +74,7 @@ int main(int argc, char** argv)
 
     
 //Transforma as cores para Preto e Branco
-    for(int i=0; i<pic.width*pic.height; i++) {
+    for(int i=0; i<pic.dimension.width*pic.dimension.height; i++) {
         int pix =(pic.img[i].r*0.3 +pic.img[i].g*0.59 + pic.img[i].b*0.11);
         pic.img[i].r = 255 - pix;
         pic.img[i].g = 255 - pix;
@@ -57,61 +82,46 @@ int main(int argc, char** argv)
     }
 
      //regra de 3
-    int tOriWidth= pic.width;
-    int tOriHeight = pic.height;
+    int tOriWidth= pic.dimension.width;
+    int tOriHeight = pic.dimension.height;
     
-    int tModWidth = ((0.5*tOriWidth)/1.0);
-    int tModHeight = ((0.5*tOriHeight)/1.0);
+    
+    
     Img pic2;  
+    pic2.dimension.width= ((tOriWidth/1.0)*0.5);
+    pic2.dimension.height= ((tOriHeight/1.0)*0.5);
+    
     load(argv[1], &pic2);
-    pic2.width= tModWidth;
-    pic2.height= tModHeight;
+    reduceToFrom(&pic2, &pic);
 
 
     bool trigger = false;
     int j = 0;
-    int diff = pic.width - pic2.width;
+    int diff = pic.dimension.width - pic2.dimension.width;
+    int resize = pic.dimension.width/pic2.dimension.width;
 
-    for(int i=0;i<pic2.width*pic2.height;i++){
-        if(i%pic2.width !=0){
-            
-        }
-    }
-    for(int i = 0, l = 0; i < pic.width * pic.height; i++, l++){
-        if( l >= pic.width ){
-            l = 0;
-            trigger = true;
-        } else if (l > pic2.width){
-            trigger = false;
-        }
-
-        if(trigger && j < pic2.width * pic2.height){
-            pic2.img[i].r = pic.img[j].r;
-            pic2.img[i].g = pic.img[j].g;
-            pic2.img[i].b = pic.img[j].b;
-            j++;
-        }
-    }
-
-    
-    // int resize = pic.width/tModWidth;
-    
-    // // tentativa de resize
-    // int i=0;
-    // while(i<(tModWidth*tModHeight)){
-    //     pic2.img[i].r = pic.img[i].r;
-    //     pic2.img[i].g = pic.img[i].g;
-    //     pic2.img[i].b = pic.img[i].b;
-    //     i+= resize;
+    // for(int i=0;i<pic2.width*pic2.height;i++,j++){
+    //      //if(i%pic.width !=0){
+    //         pic2.img[i].r = pic.img[j].r;
+    //         pic2.img[i].g = pic.img[j].g;
+    //         pic2.img[i].b = pic.img[j].b;
+    //     //} 
     // }
+    // for(int i = 0, l = 0; i < pic.width * pic.height; i++, l++){
+    //     if( l >= pic.width ){
+    //         l = 0;
+    //         trigger = true;
+    //     } else if (l > pic2.width){
+    //         trigger = false;
+    //     }
 
-    // for(int i=0; i<(tModWidth*tModHeight)-1; i++){
-    //     pic2.img[i].r = pic.img[i].r;
-    //     pic2.img[i].g = pic.img[i].g;
-    //     pic2.img[i].b = pic.img[i].b; 
+    //     if(trigger && j < pic2.width * pic2.height){
+    //         pic2.img[i].r = pic.img[j].r;
+    //         pic2.img[i].g = pic.img[j].g;
+    //         pic2.img[i].b = pic.img[j].b;
+    //         j++;
+    //     }
     // }
-    
-
 
     // Inverte as cores
     // for(int i=0; i<pic.width*pic.height; i++) {
@@ -122,7 +132,7 @@ int main(int argc, char** argv)
 
     // Exemplo: gravando um arquivo de saída com a imagem (não é necessário para o trabalho, apenas
     // para ver o resultado intermediário, por ex, da conversão para tons de cinza)
-    SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic2.width, pic2.height,
+    SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic2.dimension.width, pic2.dimension.height,
         3, (const unsigned char*) pic2.img);
 
     // Exemplo: gravando um arquivo saida.html
@@ -140,8 +150,8 @@ int main(int argc, char** argv)
     fprintf(arq,"<style> pre  {color: white;font-family: Courier;font-size: 8px;}</style>\n");
     fprintf(arq,"<pre>");
     printf("Primeiros 10 pixels da imagem:\n");
-      for(int i=0; i<tModWidth*tModHeight; i++) {
-        if(i == tModWidth){
+      for(int i=0; i<pic2.dimension.width* pic2.dimension.height; i++) {
+        if(i == pic2.dimension.width){
             fprintf(arq,"[%d %d %d] \n", pic2.img[i].r, pic2.img[i].g, pic2.img[i].b);
         }
         fprintf(arq,"[%d %d %d]", pic2.img[i].r, pic2.img[i].g, pic2.img[i].b);
