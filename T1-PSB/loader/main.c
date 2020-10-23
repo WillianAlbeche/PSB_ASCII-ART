b4cc0d43d4ae486fcd09f46944477ea791281b92
willian.albeche <willian.albeche@edu.pucrs.br>
2020-10-23 02:45:02 -0300

    finalização do codigo com debitos tecnicos

--- T1-PSB/loader/main.c
+++ T1-PSB/loader/main.c
@@ -6,9 +6,7 @@
 #include "SOIL.h"
 #include <math.h>
 
-typedef struct {
-    int width, height, totalSize;
-} Dimension; 
+ 
 // Um pixel RGB
 typedef struct {
     unsigned char r, g, b;
@@ -16,55 +14,88 @@ typedef struct {
 
 // Uma imagem em RGB
 typedef struct {
-    Dimension dimension;
-    //int width, height;
+   
+    int width, height, totalSize;
     RGB* img;
 } Img;
 
-void reduceToFrom(Img* reducedPic, Img* realPic);
-
-void reduceToFrom(Img* reducedPic, Img* realPic) {
-    printf("Reduzindo img\n");
-    reducedPic->img[reducedPic->dimension.totalSize - 1] = realPic->img[realPic->dimension.totalSize - 1];
-    int realPicLinePosition = 0;
-    int realPicColumnPosition = 0;
-    RGB (* reducedImage)[reducedPic->dimension.width] = (RGB(*)[reducedPic->dimension.width]) reducedPic->img;
-    RGB (* realImage)[realPic->dimension.width] = (RGB(*)[realPic->dimension.width]) realPic->img;
-    float widthRelation =  (realPic->dimension.width / (float) reducedPic->dimension.width);
-    float heightRelation =  (realPic->dimension.height / (float) reducedPic->dimension.height);
-    for(int line = 0; line < reducedPic->dimension.height - 1; line++) {
-        realPicLinePosition = (int) round(heightRelation * line);
-        for(int column = 0; column < reducedPic->dimension.width; column++) {
-            realPicColumnPosition = (int) round(widthRelation * column);
-            reducedImage[line][column] = realImage[realPicLinePosition][realPicColumnPosition];
-        }
-    }
-}
-
 // Protótipos
 void load(char* name, Img* pic);
+void resizeImg(Img* resized , Img* oriPic);
+char* asciiSelector(int escalaGray);
 
 // Carrega uma imagem para a struct Img
 void load(char* name, Img* pic)
 {
     int chan;
-    pic->img = (unsigned char*) SOIL_load_image(name, &pic->dimension.width, &pic->dimension.height, &chan, SOIL_LOAD_RGB);
+    pic->img = (unsigned char*) SOIL_load_image(name, &pic->width, &pic->height, &chan, SOIL_LOAD_RGB);
+    //pic->totalSize= pic->height* pic->width;
     if(!pic->img)
     {
         printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
         exit(1);
     }
-    printf("Load: %d x %d x %d\n", pic->dimension.width, pic->dimension.height, chan);
+    printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
 }
+
+
+void resizeImg(Img* resized , Img* oriPic){
+    resized->img[resized->totalSize - 1] = oriPic->img[oriPic->totalSize - 1];
+    RGB (* resizedImage)[resized->width] = (RGB(*)[resized->width]) resized->img;
+    RGB (* oriImage)[oriPic->width] = (RGB(*)[oriPic->width]) oriPic->img;
+
+
+    double widthRelation = (oriPic->width / (double) resized->width);
+    double heightRelation = (oriPic->height / (double) resized->height);
+
+    int originalPicLine = 0;
+    int originalPicColum = 0;
+
+
+    for(int i = 0; i < resized->height; i++){
+        originalPicLine = round(i * heightRelation);
+        for(int j = 0; j < resized->width; j++){
+            originalPicColum = round(j * widthRelation);
+            resizedImage[i][j] = oriImage[originalPicLine][originalPicColum];
+        }
+    }
+
+}
+char* asciiSelector(int escalaGray){
+    if( escalaGray >= 0 && escalaGray < 32){
+        return ".";
+    } else if(escalaGray < 64){
+        return ":";
+    } else if(escalaGray < 96){
+        return "c";
+    } else if(escalaGray < 128){
+        return "o";
+    } else if(escalaGray < 160){
+        return "C";
+    } else if(escalaGray < 192){
+        return "0";
+    } else if(escalaGray < 224){
+        return "8";
+    } 
+    return "@";
+}
+
 
 int main(int argc, char** argv)
 {
     Img pic;
-    if(argc == 1) {
-        printf("loader [img]\n");
-        exit(1);
-    }
-    load(argv[1], &pic);
+    // if(argc == 1) {
+    //     printf("loader [img]\n");
+    //     exit(1);
+    // }
+
+    char nomeImagem[100];
+
+    printf(" Digite o nome da imagem seguido do tipo de arquivo. por exemplo: teste.jpg\n");
+    printf("Digite: \n");
+    scanf("%s", nomeImagem);
+
+    load(nomeImagem, &pic);
 
     printf("Primeiros 10 pixels da imagem:\n");
      for(int i=0; i<10; i++) {
@@ -72,67 +103,36 @@ int main(int argc, char** argv)
      }
     printf("\n");
 
-    
-//Transforma as cores para Preto e Branco
-    for(int i=0; i<pic.dimension.width*pic.dimension.height; i++) {
-        int pix =(pic.img[i].r*0.3 +pic.img[i].g*0.59 + pic.img[i].b*0.11);
-        pic.img[i].r = 255 - pix;
-        pic.img[i].g = 255 - pix;
-        pic.img[i].b = 255 - pix;  
+    printf("Digite o valor de redimensionamento, por exemplo: para obter 50'%' da imagem, digite 0.5, para obter 80'%' da imagem, digite 0.8 e assim por diante\n");
+    double porcentagem;
+    scanf("%lf",&porcentagem);
+
+    //Transforma as cores para Preto e Branco
+    int pix;
+    for(int i=0; i<pic.width*pic.height; i++) {
+        pix =(pic.img[i].r*0.3 +pic.img[i].g*0.59 + pic.img[i].b*0.11);
+        pic.img[i].r = pix;
+        pic.img[i].g = pix;
+        pic.img[i].b = pix;  
     }
 
      //regra de 3
-    int tOriWidth= pic.dimension.width;
-    int tOriHeight = pic.dimension.height;
-    
-    
-    
-    Img pic2;  
-    pic2.dimension.width= ((tOriWidth/1.0)*0.5);
-    pic2.dimension.height= ((tOriHeight/1.0)*0.5);
-    
+    Img pic2;// imagem reduzida  
     load(argv[1], &pic2);
-    reduceToFrom(&pic2, &pic);
+
+    pic2.width= ((porcentagem*pic.width)/1.0);
+    pic2.height= ((porcentagem*pic.height)/1.0);
+    pic2.totalSize= pic2.height*pic2.width;
 
 
-    bool trigger = false;
-    int j = 0;
-    int diff = pic.dimension.width - pic2.dimension.width;
-    int resize = pic.dimension.width/pic2.dimension.width;
-
-    // for(int i=0;i<pic2.width*pic2.height;i++,j++){
-    //      //if(i%pic.width !=0){
-    //         pic2.img[i].r = pic.img[j].r;
-    //         pic2.img[i].g = pic.img[j].g;
-    //         pic2.img[i].b = pic.img[j].b;
-    //     //} 
-    // }
-    // for(int i = 0, l = 0; i < pic.width * pic.height; i++, l++){
-    //     if( l >= pic.width ){
-    //         l = 0;
-    //         trigger = true;
-    //     } else if (l > pic2.width){
-    //         trigger = false;
-    //     }
-
-    //     if(trigger && j < pic2.width * pic2.height){
-    //         pic2.img[i].r = pic.img[j].r;
-    //         pic2.img[i].g = pic.img[j].g;
-    //         pic2.img[i].b = pic.img[j].b;
-    //         j++;
-    //     }
-    // }
-
-    // Inverte as cores
-    // for(int i=0; i<pic.width*pic.height; i++) {
-    //     pic.img[i].r = 255 - pic.img[i].r;
-    //     pic.img[i].g = 255 - pic.img[i].g;
-    //     pic.img[i].b = 255 - pic.img[i].b;
-    // }
+    RGB resizedImage[pic.width * pic.height];
+    pic2.img = resizedImage;
+    
+    resizeImg(&pic2 , &pic);
 
     // Exemplo: gravando um arquivo de saída com a imagem (não é necessário para o trabalho, apenas
     // para ver o resultado intermediário, por ex, da conversão para tons de cinza)
-    SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic2.dimension.width, pic2.dimension.height,
+    SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic2.width, pic2.height,
         3, (const unsigned char*) pic2.img);
 
     // Exemplo: gravando um arquivo saida.html
@@ -146,21 +146,26 @@ int main(int argc, char** argv)
     fprintf(arq, "<html>\n");
     fprintf(arq,"<head>\n");
     fprintf(arq,"</head>\n");
-    fprintf(arq,"<body style=""background: black;"" leftmargin=0 topmargin=0>\n");
     fprintf(arq,"<style> pre  {color: white;font-family: Courier;font-size: 8px;}</style>\n");
+    fprintf(arq,"<body style='background: black;' leftmargin=0 topmargin=0>\n");
     fprintf(arq,"<pre>");
-    printf("Primeiros 10 pixels da imagem:\n");
-      for(int i=0; i<pic2.dimension.width* pic2.dimension.height; i++) {
-        if(i == pic2.dimension.width){
-            fprintf(arq,"[%d %d %d] \n", pic2.img[i].r, pic2.img[i].g, pic2.img[i].b);
+    RGB (* resizedPixels)[pic2.width] = (RGB(*)[pic2.width]) pic2.img;  
+     int add= 0;
+    for(int line = 0; line + 5 <= pic2.height; line += 5) {
+        for (int column = 0; column + 4 <= pic2.width; column += 4) {
+            for (int setorLinha = line; setorLinha < line + 5; setorLinha++) {
+                for (int setorColuna = column; setorColuna < column + 4; setorColuna++) {
+                    add += resizedPixels[setorLinha][setorColuna].r;
+                }
+            }
+            fprintf(arq,asciiSelector((int) round(add / 20.0)));
+            add = 0;
         }
-        fprintf(arq,"[%d %d %d]", pic2.img[i].r, pic2.img[i].g, pic2.img[i].b);
-      }
-     printf("\n");
-     fprintf(arq,"</pre>\n");                
-     fprintf(arq,"</body>\n");
-     fclose(arq);
-    // printf("\n");
+        fprintf(arq,"\n");
+    }
+    fprintf(arq,"</pre>\n");                
+    fprintf(arq,"</body>\n");
+    fclose(arq);
     free(pic.img);
     free(pic2.img);
 }
